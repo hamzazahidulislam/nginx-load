d2087d804237f3ebd491e0bd1a83c1e989f8c81b
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2022-01-08 12:26:43 +0600

    build docker image

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 /** @format */
+const express = require('express')
+
+const app = express()
+const appId = process.env.APP_ID
+
+app.get('/', (req, res) => res.send(`App listen on port ${appId}`))
+
+app.listen(appId, () => console.log(`App listen on port ${appId}`))
